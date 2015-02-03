e004b4e4fcbebb58d37339d59d73c2f86effb192
xiaotie <wudixiaotie@gmail.com>
2015-02-03 15:35:04 +0800

    ws_server

--- first_blood/src/http_server.erl
+++ first_blood/src/http_server.erl
@@ -1,30 +1,31 @@
 -module (http_server).
 
 -export ([start/0, start/1, stop/0]).
+-compile(export_all).
 
 -define (DEFAULT_PORT, 1987).
--define (DEFAULT_ACCPEPTOR_COUNT, 10).
--define (DEF_PORT, 1987).
--define (DEF_PORT, 1987).
--define (DEF_PORT, 1987).
--define (DEF_PORT, 1987).
--define (DEF_PORT, 1987).
+% -define (DEF_PORT, 1987).
+% -define (DEF_PORT, 1987).
+% -define (DEF_PORT, 1987).
+% -define (DEF_PORT, 1987).
+% -define (DEF_PORT, 1987).
 
 % http://erlang.org/doc/apps/inets/http_server.html
 % spawn_opt是啥？
 % erlang:system_info(schedulers)是啥？
+% CPU调度器，用来调度多核cpu的使用。
 start() ->
     start(?DEFAULT_PORT).
 
 start(Port) ->
     io:format("start_server: http://localhost:~p~n", [Port]),
-    register(ws_server, spawn(fun() -> listen(Port, ?DEFAULT_ACCPEPT_COUNT) end)).
+    listen(Port),
+    register(http_server, self()),
+    receive
+        _ -> stop
+    end.
 
-start(Port, AcceptorCount) ->
-    io:format("start_server: http://localhost:~p~n", [Port]),
-    register(ws_server, spawn(fun() -> listen(Port, AcceptorCount) end)).
-
-listen(Port, AcceptorCount) ->
+listen(Port) ->
     Opts = [{active, false},
             binary,
             {backlog, 512},
@@ -34,39 +35,53 @@ listen(Port, AcceptorCount) ->
             %%{nodelay,true},
             {reuseaddr, true}],
     {ok, ListenSocket} = gen_tcp:listen(Port, Opts),
-    Fun = fun(I) ->
-                    register(list_to_atom("acceptor_" ++ integer_to_list(I)),
-                             spawn_opt(?MODULE, accept, [S, I], [link, {scheduler, I}]))
-            end,
-    lists:foreach(Fun, lists:seq(1, AcceptorCount)).
+    Fun = fun(SchedulerIndex) ->
+        io:format("SchedulerIndex:~p~n", [SchedulerIndex]),
+        register(list_to_atom("acceptor_" ++ integer_to_list(SchedulerIndex)),
+                 spawn_opt(fun() -> accept(ListenSocket, SchedulerIndex) end,
+                           [link, {scheduler, SchedulerIndex}]))
+    end,
+    lists:foreach(Fun, lists:seq(1, erlang:system_info(schedulers))).
 
-accept(ListenSocket) ->
-    io:format("accept pid:~p~n", [self()]),
+accept(ListenSocket, SchedulerIndex) ->
+    io:format("accept pid:~p, scheduler index:~p~n", [self(), SchedulerIndex]),
     case gen_tcp:accept(ListenSocket) of
         {ok, Socket} ->
-            spawn(fun() -> accept(ListenSocket) end),
-            loop(Socket);
-        {error, Why} ->
-            io:format("ListenSocket stoped:~p~n", [Why])
-    end.
+            spawn_opt(fun() -> loop(Socket) end, [link, {scheduler, SchedulerIndex}]);
+        Error ->
+            erlang:error(Error)
+    end,
+    accept(ListenSocket, SchedulerIndex).
 
 loop(Socket) ->
-    receive
-        {tcp, Socket, Bin} ->
-            io:format("Server received binary = ~p~n", [Bin]),
-            case erlang:decode_packet(http, Bin, [{packet_size, 0}]) of
-                {ok, HttpRequest, _Rest} ->
-                    io:format("Server received HttpRequest = ~p~n", [HttpRequest]),
-                    gen_tcp:send(Socket, "fuck"),
-                    gen_tcp:close(Socket),
-                    ok;
-                {error, Why} ->
-                    {error, Why} 
-            end;
-        {tcp_closed, Socket} ->
-            io:format("Server socket closed.~n")
+    case gen_tcp:recv(Socket, 0) of
+        {ok, http_eoh} ->
+            Response = <<"HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nhello world!">>,
+            gen_tcp:send(Socket, Response),
+            gen_tcp:close(Socket),
+            ok;
+        {ok, _Data} ->
+            loop(Socket);
+        Error ->
+            Error
     end.
+    % receive
+    %     {tcp, Socket, Bin} ->
+    %         io:format("Server received binary = ~p~n", [Bin]),
+    %         case erlang:decode_packet(http, Bin, [{packet_size, 0}]) of
+    %             {ok, HttpRequest, _Rest} ->
+    %                 io:format("Server received HttpRequest = ~p~n", [HttpRequest]),
+    %                 gen_tcp:send(Socket, "fuck"),
+    %                 gen_tcp:close(Socket),
+    %                 ok;
+    %             {error, Why} ->
+    %                 {error, Why} 
+    %         end;
+    %     {tcp_closed, Socket} ->
+    %         io:format("Server socket closed.~n")
+    % end.
 
 
 stop() ->
+    http_server ! stop,
     io:format("start_server: http://localhost:").
